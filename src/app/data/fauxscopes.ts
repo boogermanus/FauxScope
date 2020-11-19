645a766a4dde1b88e768ed0d3bcc057c45b91507
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-11-18 20:37:00 -0600

    getting the data right

--- src/app/data/fauxscopes.ts
+++ src/app/data/fauxscopes.ts
@@ -6,43 +6,15 @@ const fauxscopes: IScope[] = [
         signImageUrl: 'assets/Aries.svg',
         meaning: 'The Ram',
         astrologicalStartDay: 21,
-        astrologicalStartMonth: 2,
-        astrologicalEndDay: 20,
-        astrologicalEndMonth: 3,
-        astronomicalStartDay: 19,
-        astronomicalStartMonth: 3,
-        astronomicalEndDay: 14,
-        astronomicalEndMonth: 4,
-        duration: 38
-    },
-    {
-        sign: 'Taurus',
-        signImageUrl: 'assets/Taurus.svg',
-        meaning: 'The Bull',
-        astrologicalStartDay: 21,
         astrologicalStartMonth: 3,
-        astrologicalEndDay: 21,
+        astrologicalEndDay: 20,
         astrologicalEndMonth: 4,
-        astronomicalStartDay: 15,
-        astronomicalStartMonth: 3,
-        astronomicalEndDay: 21,
+        astronomicalStartDay: 19,
+        astronomicalStartMonth: 4,
+        astronomicalEndDay: 14,
         astronomicalEndMonth: 5,
         duration: 25
     },
-    {
-        sign: 'Gemini',
-        signImageUrl: 'assets/Gemini.svg',
-        meaning: 'The Twins',
-        astrologicalStartDay: 22,
-        astrologicalStartMonth: 4,
-        astrologicalEndDay: 21,
-        astrologicalEndMonth: 5,
-        astronomicalStartDay: 22,
-        astronomicalStartMonth: 5,
-        astronomicalEndDay: 20,
-        astronomicalEndMonth: 6,
-        duration: 30
-    }
 ];
 
 export { fauxscopes };
