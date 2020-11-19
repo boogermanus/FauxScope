21d3c61fb789018a3b263d8eeb98e1facc1f2999
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-11-18 20:13:35 -0600

    add iscope

---
+++ src/app/models/iscope.ts
@@ -0,0 +1,10 @@
+export interface IScope {
+    sign: string;
+    signImageUrl: string;
+    meaning: string;
+    astrologicalMonth: number;
+    astrologicalDay: number;
+    astronomicalMonth: number;
+    astronomicalDay: number;
+    duration: number;
+}
