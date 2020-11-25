feb6414b968aaeb9bf1ce2964a27eda5b8801eef
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-11-24 20:06:10 -0600

    add some new material components

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
-
-
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatCardModule
+    MatCardModule,
+    MatSelectModule,
+    MatFormFieldModule,
   ],
   exports: [
     MatCardModule,
+    MatSelectModule,
+    MatFormFieldModule,
   ]
 })
 export class MaterialModule { }
