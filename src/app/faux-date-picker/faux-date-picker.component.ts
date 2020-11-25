7bfa5eb2d8c96e613e716d4035c260fd75b25fd9
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-11-24 20:06:16 -0600

    more layout

--- src/app/faux-date-picker/faux-date-picker.component.ts
+++ src/app/faux-date-picker/faux-date-picker.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FauxDatePickerComponent implements OnInit {
 
+  months: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   constructor() { }
 
   ngOnInit(): void {
