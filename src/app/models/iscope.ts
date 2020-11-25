5da47cee147ba99831a604d70558d4f90ccf6d5f
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-11-25 15:51:38 -0600

    make stuff optional

--- src/app/models/iscope.ts
+++ src/app/models/iscope.ts
@@ -2,13 +2,13 @@ export interface IScope {
     sign: string;
     signImageUrl: string;
     meaning: string;
-    astrologicalStartDay: number;
-    astrologicalStartMonth: number;
-    astrologicalEndDay: number;
-    astrologicalEndMonth: number;
-    astronomicalStartDay: number;
-    astronomicalStartMonth: number;
-    astronomicalEndDay: number;
-    astronomicalEndMonth: number;
+    astrologicalStartDay?: number;
+    astrologicalStartMonth?: number;
+    astrologicalEndDay?: number;
+    astrologicalEndMonth?: number;
+    astronomicalStartDay?: number;
+    astronomicalStartMonth?: number;
+    astronomicalEndDay?: number;
+    astronomicalEndMonth?: number;
     duration: number;
 }
