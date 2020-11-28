ebf018f7c71daa1ff04955c4918d52322fcbd171
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-11-28 13:32:00 -0600

    add new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FauxDatePickerComponent } from './faux-date-picker/faux-date-picker.component';
 import { MaterialModule } from './material.module';
 import { FauxScopeViewComponent } from './faux-scope-view/faux-scope-view.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FauxDatePickerComponent,
-    FauxScopeViewComponent
+    FauxScopeViewComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
