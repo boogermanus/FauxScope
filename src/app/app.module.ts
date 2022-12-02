5fcb88e5352eb16dd3febba045f5d935fc3439f7
jordan.woodruff <jordan.woodruff@tylertech.com>
2022-12-02 16:17:40 -0600

    compiling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FauxDatePickerComponent } from './faux-date-picker/faux-date-picker.component';
+import { MaterialModule } from './material.module';
+import { FauxScopeViewComponent } from './faux-scope-view/faux-scope-view.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FauxDatePickerComponent,
+    FauxScopeViewComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/iscope.ts
@@ -0,0 +1,14 @@
+export interface IScope {
+    sign: string;
+    signImageUrl: string;
+    meaning: string;
+    astrologicalStartDay: number | null;
+    astrologicalStartMonth: number | null;
+    astrologicalEndDay: number | null;
+    astrologicalEndMonth: number | null;
+    astronomicalStartDay: number | null;
+    astronomicalStartMonth: number | null;
+    astronomicalEndDay: number | null;
+    astronomicalEndMonth: number | null;
+    duration: number;
+}
